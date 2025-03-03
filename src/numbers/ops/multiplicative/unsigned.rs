37cd48f9cbe099e3a49f6876032f430ad197ecf4
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2025-03-03 23:31:54 +0530

    Created helper substraction function for division.

--- src/numbers/ops/multiplicative/unsigned.rs
+++ src/numbers/ops/multiplicative/unsigned.rs
@@ -1,10 +1,13 @@
-use crate::numbers::Integer;
+use crate::numbers::{
+	Integer,
+	INTEGER_BASE,
+};
 
 pub(crate) fn unsigned_integer_mul(lhs: &Integer, rhs: &Integer) -> Integer {
 	let mut result = Integer::new(0);
 	for (i, d1) in rhs.digits.iter().enumerate() {
 		let mut digits = Vec::with_capacity(i + lhs.digits.len() + 1);
-		for j in 0..i {
+		for _ in 0..i {
 			digits.push(0);
 		}
 		let mut rem = 0u32;
@@ -20,4 +23,21 @@ pub(crate) fn unsigned_integer_mul(lhs: &Integer, rhs: &Integer) -> Integer {
 	}
 
 	result
+}
+
+fn slice_integer_sub(lhs: &mut [u32], rhs: &[u32]) {
+	let mut carry: u64 = 0;
+	for i in 0..rhs.len() {
+		let reg = carry + rhs[i] as u64;
+		let l = lhs[i] as u64;
+		if l >= reg {
+			lhs[i] = (l - reg) as u32;
+		} else {
+			lhs[i] = (l + INTEGER_BASE - reg) as u32;
+			carry = 1;
+		}
+	}
+	if carry > 0 {
+		lhs[lhs.len() - 1] -= carry as u32;
+	}
 }
