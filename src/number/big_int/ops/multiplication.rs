181494391dcc6baa7a616b056f3b488822be1cca
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2025-03-05 15:21:21 +0530

    Further optimized multiplication to add the result of multiplication of each digit in the result vector itself.

--- src/number/big_int/ops/multiplication.rs
+++ src/number/big_int/ops/multiplication.rs
@@ -1,43 +1,31 @@
 use crate::number::BigInt;
 
-#[inline]
-fn add_slice(lhs: &mut [u32], rhs: &[u32]) {
-	let mut carry: u64 = 0;
-	for i in 0..rhs.len() {
-		let sum = lhs[i] as u64 + rhs[i] as u64 + carry;
-		lhs[i] = sum as u32;
-		carry = sum >> 32;
-	}
-	for i in rhs.len()..lhs.len() {
-		if carry == 0 {
-			break;
-		} else {
-			let sum = lhs[i] as u64 + carry;
-			lhs[i] = sum as u32;
-			carry = sum >> 32;
-		}
-	}
-}
-
 impl BigInt {
 	pub(crate) fn unsigned_mul(&self, rhs: &BigInt) -> BigInt {
 		let mut result = vec![0; self.digits.len() + rhs.digits.len()];
-		let mut digits = Vec::with_capacity(self.digits.len() + 1);
 		for (i, d1) in rhs.digits.iter().enumerate() {
-			let mut rem = 0u32;
-			for d2 in self.digits.iter() {
-				let reg = *d2 as u64 * *d1 as u64 + rem as u64;
-				rem = (reg >> 32) as u32;
-				digits.push(reg as u32);
+			let mut carry = 0u64;
+			for j in 0..self.digits.len() {
+				let k = i + j;
+				let sum = self.digits[j] as u64 * *d1 as u64 + carry + result[k] as u64;
+				carry = sum >> 32;
+				result[k] = sum as u32;
 			}
-			if rem != 0 {
-				digits.push(rem);
+			for j in (self.digits.len() + i)..result.len() {
+				if carry == 0 {
+					break;
+				} else {
+					let sum = carry + result[j] as u64;
+					result[j] = sum as u32;
+					carry = sum >> 32;
+				}
 			}
-			add_slice(&mut result[i..], &digits);
-			digits.clear();
 		}
 
-		BigInt::new(result)
+		let mut result = BigInt::new(result);
+		result.trim();
+
+		result
 	}
 }
 
